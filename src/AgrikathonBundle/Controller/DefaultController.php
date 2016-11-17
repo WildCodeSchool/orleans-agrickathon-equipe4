5d90b6f21398d8ea2aad85a588b7105e7147262a
Kim Nguyen <knguyen.wcs@gmail.com>
2016-11-17 09:17:26 +0100

    crea fonctionnalités site

--- src/AgrikathonBundle/Controller/DefaultController.php
+++ src/AgrikathonBundle/Controller/DefaultController.php
@@ -8,10 +8,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/index", name="index")
      */
     public function indexAction()
     {
         return $this->render('AgrikathonBundle:Default:index.html.twig');
     }
+
+    /**
+    **
+    * @Route("/liste",  name="liste")
+    */
+    public function listeAction()
+    {
+        return $this->render('AgrikathonBundle:Default:listereponse.html.twig');
+    }
+
+    /**
+     **
+     * @Route("/depot",  name="depot")
+     */
+    public function depotAction()
+    {
+        return $this->render('AgrikathonBundle:Default:depotann.html.twig');
+    }
+
+
+    /**
+     **
+     * @Route("/login1",  name="login1")
+     */
+    public function loginAction()
+    {
+        return $this->render(':FosUserSecurity:login.html.twig');
+    }
+
 }
